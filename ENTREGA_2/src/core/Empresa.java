1e17ceb105de411cc477db68945e6a2bfba83c86
catalinavillacris <m.villacris@uniandes.edu.co>
2023-10-15 20:48:51 -0500

    Segundo commit

--- ENTREGA_2/src/core/Empresa.java
+++ ENTREGA_2/src/core/Empresa.java
@@ -1,5 +1,15 @@
 package core;
 
 public class Empresa {
-
+	
+	private Inventario inventario;
+	private SRA sra;
+	private HashMap<String,Sede> sedes;
+	
+	public void Empresa() {
+		this.inventario=new Inventario();
+		this.sra= new SRA();
+		this.sedes=new HashMap<String,Sede>();
+		
+	}
 }
--- ENTREGA_2/src/core/Inventario.java
+++ ENTREGA_2/src/core/Inventario.java
@@ -1,5 +1,23 @@
 package core;
 
+import java.util.HashMap;
+
 public class Inventario {
+	
+	private HashMap<Categoria,Vehiculo> catalogo;
+	private HashMap<String,Vehiculo> mantenimiento;
+	
+	public void Inventario() {
+		private HashMap<Categoria,Vehiculo> catalogo;
+		private HashMap<String,Vehiculo> mantenimiento;
+	}
+	
+	public HashMap<Categoria,Vehiculo> getCatalogo(){
+		return this.catalogo;
+	}
+	
+	public HashMap<String,Vehiculo> getMantenimiento(){
+		return this.mantenimiento;
+	}
 
 }
